3804995c45d04aca85a7519cbc631472b2d91868
jaykumarjp <42172283+jaykumarjp@users.noreply.github.com>
2020-02-14 19:04:47 +0530

    created one api

---
+++ vapi/one.php
@@ -0,0 +1,9 @@
+<?php
+if ($_GET['id']==123)
+	print 'Name : Avinash Jain<br>Credit Card : 2544987863893669';
+else
+	if ($_GET['id']==124)
+	print 'Name : Shahid Khan<br>Credit Card : 6799859617806996';
+else
+	print 'Invalid ID given.';
+?>
